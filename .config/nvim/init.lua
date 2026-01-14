4ce443e5d7844dfe05c0eac98d601ee9829b35d8
jourdanhaines <58449338+jourdanhaines@users.noreply.github.com>
2026-01-14 19:18:59 +1100

    feat: move keymaps from init to keymaps.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -7,15 +7,3 @@ vim.o.expandtab = true
 vim.o.softtabstop = 4
 vim.o.number = true
 vim.o.relativenumber = true
-
-vim.keymap.set("n", "ff", function()
-	require("fzf-lua").files({
-		cwd = vim.fn.getcwd(),
-		git_icons = true,
-		file_icons = true
-	})
-end, { desc = "Project Files" })
-
-vim.keymap.set("n", "fg", function()
-	require("fzf-lua").live_grep()
-end, { desc = "Live Grep" })
--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,27 +1,43 @@
 vim.api.nvim_create_autocmd("LspAttach", {
-  group = vim.api.nvim_create_augroup("UserLspKeymaps", { clear = true }),
-  callback = function(ev)
-    local opts = { buffer = ev.buf, silent = true, noremap = true }
+    group = vim.api.nvim_create_augroup("UserLspKeymaps", { clear = true }),
+    callback = function(ev)
+        local opts = { buffer = ev.buf, silent = true, noremap = true }
 
-    -- Go to definition
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, vim.tbl_extend("force", opts, { desc = "Go to definition" }))
-    
-    -- Hover intellisense
-    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        -- Go to definition
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, vim.tbl_extend("force", opts, { desc = "Go to definition" }))
 
-    -- Rename
-    vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
+        -- Hover intellisense
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
 
-    -- Format
-    vim.keymap.set("n", "<space>f", function()
-        vim.lsp.buf.format { async = true }
-    end, opts)
+        -- Rename
+        vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
 
-    -- (optional, nice to have)
-    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, vim.tbl_extend("force", opts, { desc = "Go to declaration" }))
-    vim.keymap.set("n", "gr", vim.lsp.buf.references, vim.tbl_extend("force", opts, { desc = "References" }))
-    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, vim.tbl_extend("force", opts, { desc = "Implementation" }))
-    vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, vim.tbl_extend("force", opts, { desc = "Type definition" }))
-  end,
+        -- Format
+        vim.keymap.set("n", "<space>f", function()
+            vim.lsp.buf.format { async = true }
+        end, opts)
+
+        -- (optional, nice to have)
+        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, vim.tbl_extend("force", opts, { desc = "Go to declaration" }))
+        vim.keymap.set("n", "gr", vim.lsp.buf.references, vim.tbl_extend("force", opts, { desc = "References" }))
+        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, vim.tbl_extend("force", opts, { desc = "Implementation" }))
+        vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, vim.tbl_extend("force", opts, { desc = "Type definition" }))
+    end,
 })
+
+-- Global keymaps (always available)
+
+-- Open fzf for current directory
+vim.keymap.set("n", "ff", function()
+    require("fzf-lua").files({
+        cwd = vim.fn.getcwd(),
+        git_icons = true,
+        file_icons = true
+    })
+end, { desc = "Project Files" })
+
+-- Open live grep
+vim.keymap.set("n", "fg", function()
+    require("fzf-lua").live_grep()
+end, { desc = "Live Grep" })
 
