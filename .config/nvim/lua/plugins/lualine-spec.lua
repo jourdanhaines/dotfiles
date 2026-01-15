d94c8bbf0b3a2c343fce7c8f4f56198d362c585a
jourdanhaines <58449338+jourdanhaines@users.noreply.github.com>
2026-01-15 16:30:53 +1100

    feat: show relative project file path in lualine

--- .config/nvim/lua/plugins/lualine-spec.lua
+++ .config/nvim/lua/plugins/lualine-spec.lua
@@ -1,9 +1,43 @@
 return {
     "nvim-lualine/lualine.nvim",
     config = function()
+        local function get_git_root()
+            if vim.b.lualine_git_root ~= nil then
+                return vim.b.lualine_git_root
+            end
+
+            local dir = vim.fn.expand('%:p:h')
+            local git_root = vim.fn.systemlist('git -C ' .. vim.fn.shellescape(dir) .. ' rev-parse --show-toplevel 2>/dev/null')[1]
+
+            if vim.v.shell_error == 0 and git_root then
+                vim.b.lualine_git_root = git_root
+            else
+                vim.b.lualine_git_root = ''
+            end
+
+            return vim.b.lualine_git_root
+        end
+
+        local function relative_path()
+            local filepath = vim.fn.expand('%:p')
+            if filepath == '' then
+                return '[No Name]'
+            end
+
+            local git_root = get_git_root()
+            if git_root ~= '' and vim.startswith(filepath, git_root) then
+                return filepath:sub(#git_root + 2)
+            end
+
+            return vim.fn.expand('%:t')
+        end
+
         require("lualine").setup({
             options = {
                 theme = "dracula"
+            },
+            sections = {
+                lualine_c = { relative_path }
             }
         })
     end
