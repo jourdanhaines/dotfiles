524dc02736f05fb49d4d1ae5bb021398cef470da
Jourdan Haines <58449338+jourdanhaines@users.noreply.github.com>
2026-01-30 11:08:23 +1100

    feat: add mini indentscope plugin

---
+++ .config/nvim/lua/plugins/mini-indentscope-spec.lua
@@ -0,0 +1,11 @@
+return {
+    {
+        "echasnovski/mini.indentscope",
+        version = false,
+        opts = {
+            draw = {
+                animation = function() return 0 end,
+            },
+        },
+    }
+}
