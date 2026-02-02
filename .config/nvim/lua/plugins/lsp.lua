268d9bcdee5d5d174799e4e9257137108e81ef1e
jourdanhaines <58449338+jourdanhaines@users.noreply.github.com>
2026-02-02 12:19:50 +1100

    feat: add tailwind lsp config support

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,4 +1,29 @@
 return {
-  { "neovim/nvim-lspconfig" },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+    },
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      vim.lsp.config.tailwindcss = {
+        capabilities = capabilities,
+        filetypes = {
+          "html",
+          "css",
+          "scss",
+          "javascript",
+          "javascriptreact",
+          "typescript",
+          "typescriptreact",
+          "vue",
+          "svelte",
+        },
+      }
+
+      vim.lsp.enable("tailwindcss")
+    end,
+  },
 }
 
